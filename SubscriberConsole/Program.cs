ff680675e6e3a00986f558ff08653f28a8d10e8a
shbha <shaheendhiman@gmail.com>
2018-09-27 14:43:42 -0500

    first commit

---
+++ SubscriberConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SubscriberConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var lastMessage = string.Empty;
+			var broadcastedStore = "BroadcastDataStore/WriteLines.txt";
+			//var broadcastStorage = "/data/WriteLines.txt";
+			while (true)
+			{
+				System.Threading.Thread.Sleep(5000);
+				var content = System.IO.File.ReadAllText(broadcastedStore);
+				if (lastMessage != content)
+				{
+					Console.WriteLine(content);
+					lastMessage = content;
+				}
+			}
+		}
+	}
+}
---
+++ broadcastserver/Controllers/BroadcastController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+
+namespace broadcastserver.Controllers
+{
+    public class BroadcastController : ApiController
+    {
+		[Route("broadcast/message/from/{from}/text/{message}")]
+		[HttpGet]
+		public IHttpActionResult Get(string from, string message)
+		{
+			//TODO
+
+			try
+			{
+				//var broadcastStorage = "/data/WriteLines.txt";
+			}
+			catch (Exception ex)
+			{
+				//swallow
+			}
+			File.WriteAllLines(@"//MININT-9SH87KU/Users/shbha/BroadcastDataStore/WriteLines.txt", new string[] { message });
+			
+			return this.Ok($"Message from {from} was successfully broadcasted. Message: {message}");
+			//write to file
+		}
+    }
+}
